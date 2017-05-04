63d1e8656efb4b687811a5678c9263f160006025
Patricia Sittikul <patricia.sitt@gmail.com>
2017-05-04 15:09:13 -0400

    Successfully added upload file function

--- testgui.py
+++ testgui.py
@@ -1,20 +1,39 @@
 import sys
+import tkinter
+import openpyxl
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+from tkinter import Tk
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
+UPLOAD_FOLDER = 'C:/Users/Bridget Velez/'
+ALLOWED_EXTENSIONS = 'xls'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 class Example(QWidget):
+
+    def getFile(self):
+        print ("getFile called!")
+        Tk().withdraw()
+        filename = askopenfilename()
+        print(filename)
+        data_wb = openpyxl.load_workbook(filename)
     
     def __init__(self):
         super().__init__()
         
-        self.initUI()
-        
+        self.initUI()        
         
     def initUI(self):
 
         btn = QPushButton('Upload file to prep for mail merge', self)
         btn.resize(btn.sizeHint())
         btn.move(50,50)
+        btn.clicked.connect(self.getFile)
         updateBtn = QPushButton('Upload file to update contact information', self)
         updateBtn.resize(updateBtn.sizeHint())
         updateBtn.move(350, 50)
@@ -24,8 +43,11 @@ class Example(QWidget):
     
         self.show()
 
+
+            
+
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    ex = Example()    
+    ex = Example()
     sys.exit(app.exec_())
